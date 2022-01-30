1cccdad40d15d308b3bc72f307fc7ce9b98b527f
Romazes <romazes20@gmail.com>
2022-01-30 23:15:58 +0200

    add: Struct Donation add: mapping Donations add: event for Donations feature: tests are covered all logic in contract

--- fundraiser-truffle-proj/test/fundraiser_test.js
+++ fundraiser-truffle-proj/test/fundraiser_test.js
@@ -7,19 +7,20 @@ contract("Fundraiser", (accounts) => {
   const imageURL = "https://placekitten.com/600/350";
   const description = "Beneficiary description";
   const beneficiary = accounts[1];
-  const custodian = accounts[0];
+  const owner = accounts[0];
+
+  beforeEach(async () => {
+    fundraiser = await FundraiserContract.new(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary,
+      owner
+    );
+  });
 
   describe("initialization", () => {
-    beforeEach(async () => {
-      fundraiser = await FundraiserContract.new(
-        name,
-        url,
-        imageURL,
-        description,
-        beneficiary,
-        custodian
-      );
-    });
     it("gets the beneficiary name", async () => {
       const actual = await fundraiser.name();
       assert.equal(actual, name, "names should match");
@@ -40,9 +41,86 @@ contract("Fundraiser", (accounts) => {
       const actual = await fundraiser.beneficiary();
       assert.equal(actual, beneficiary, "beneficiary addresses should match");
     });
-    it("gets the custodian", async () => {
-        const actual = await fundraiser.custodian();
-        assert.equal(actual, custodian, "custodian addresses should match");
+    it("gets the owner", async () => {
+      const actual = await fundraiser.owner();
+      assert.equal(actual, owner, "custodian addresses should match");
+    });
+  });
+  describe("setBeneficiary", () => {
+    const newBeneficiary = accounts[2];
+
+    it("updated beneficiary when called by owner account", async () => {
+      await fundraiser.setBeneficiary(newBeneficiary, { from: owner });
+      const actualBeneficiary = await fundraiser.beneficiary();
+      assert.equal(
+        actualBeneficiary,
+        newBeneficiary,
+        "beneficiaries should match"
+      );
+    });
+    it("throw an error when called from a non-owner account", async () => {
+      try {
+        await fundraiser.setBeneficiary(newBeneficiary, { from: accounts[3] });
+        assert.fail("withdraw was not restricted to owners");
+      } catch (err) {
+        const expectedError = "Ownable: caller is not the owner";
+        const actualError = err.reason;
+        assert.equal(actualError, expectedError, "should not be permitted");
+      }
+    });
+  });
+  describe("making donations", () => {
+    const value = web3.utils.toWei("0.0289");
+    const donor = accounts[2];
+
+    it("increases myDonationsCount", async () => {
+      const currentDonationsCount = await fundraiser.myDonationsCount({
+        from: donor,
       });
+
+      await fundraiser.donate({ from: donor, value });
+      const newDonationsCount = await fundraiser.myDonationsCount({
+        from: donor,
+      });
+
+      assert.equal(
+        1,
+        newDonationsCount - currentDonationsCount,
+        "myDonationsCount should increment by 1"
+      );
+    });
+    it("includes donation in myDontations", async () => {
+      await fundraiser.donate({ from: donor, value });
+      const { values, dates } = await fundraiser.myDonations({ from: donor });
+      assert.equal(value, values[0], "values should match");
+      assert(dates[0], "date should be present");
+    });
+    it("increases the totalDonations amount", async () => {
+      const currentTotalDonations = await fundraiser.totalDonations();
+      await fundraiser.donate({ from: donor, value });
+      const newTotalDonations = await fundraiser.totalDonations();
+
+      const diff = newTotalDonations - currentTotalDonations;
+
+      assert.equal(diff, value, "difference should match the donation value");
+    });
+    it("increases donationsCount", async () => {
+      const currentDonationsCount = await fundraiser.donationsCount();
+      await fundraiser.donate({ from: donor, value });
+      const newDonationsCount = await fundraiser.donationsCount();
+
+      assert.equal(
+        1,
+        newDonationsCount - currentDonationsCount,
+        "donationsCount should increment by 1"
+      );
+    });
+    it("emits the DonationReceived event", async () => {
+        const tx = await fundraiser.donate({ from: donor, value });
+        const expectedEvent = "DonationReceived";
+        const actualEvent = tx.logs[0].event;
+
+        assert.equal(actualEvent, expectedEvent, "events should match");
+    });
   });
 });
