f7697835be8c9189fb52e68ec67083070fa6c894
Romazes <romazes20@gmail.com>
2022-01-30 18:27:12 +0200

    feature: init Fundraiser contract and test

---
+++ fundraiser-truffle-proj/contracts/Fundraiser.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+contract Fundraiser {
+    string public name;
+    string public url;
+    string public imageURL;
+    string public description;
+
+    address payable public beneficiary;
+    address public custodian;
+
+    constructor(string memory _name, string memory _url, string memory _imageURL, string memory _description, address payable _beneficiary, address _custodian) {
+        name = _name;
+        url = _url;
+        imageURL = _imageURL;
+        description = _description;
+        beneficiary = _beneficiary;
+        custodian = _custodian;
+    }
+}
